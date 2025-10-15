9d2eff7f3338e6a801a90868f0ecf98c168992f7
Chrysidoidea <asuantodilae@protonmail.com>
2025-10-15 16:07:49 +0700

    feat: add mode state and enum for control, refine some code

--- Pomodoro Timer Est/ContentView.swift
+++ Pomodoro Timer Est/ContentView.swift
@@ -6,24 +6,41 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+private enum Mode {
+    case focus
+    case rest
+}
 
 struct ContentView: View {
+    @State private var mode: Mode = .focus
+
     var body: some View {
         ZStack {
-            LinearGradient(colors: [Color(red: 55/255, green: 0/255, blue: 110/255),
-                                    Color(red: 85/255, green: 0/255, blue: 155/255)],
-                           startPoint: .top,
-                           endPoint: .bottom)
-        .ignoresSafeArea()
-        .ignoresSafeArea()
+            LinearGradient(
+                colors: [
+                    Color(red: 55/255, green: 0/255, blue: 110/255),
+                    Color(red: 85/255, green: 0/255, blue: 155/255)
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+
             VStack(spacing: 14) {
                 Text("Pomodoro Timer Est")
                     .font(.title)
-                    .foregroundStyle(LinearGradient(
-                        colors: [Color(red: 255.0/255.0, green: 63.0/255.0, blue: 127.0/255.0), .blue],
-                        startPoint: .leading,
-                        endPoint: .trailing
-                    ))
+                    .foregroundStyle(
+                        LinearGradient(
+                            colors: [
+                                Color(red: 255.0/255.0, green: 63.0/255.0, blue: 127.0/255.0),
+                                .blue
+                            ],
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
             }
         }
     }
